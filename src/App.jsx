7be457211ed8e33c169d19389df8ffd39d6a4d2e
MaddalenaRen <ileniarennella@gmail.com>
2025-04-22 16:42:13 +0200

    Bio-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
   return <></>;
   // inserire "Routes" e "Route"
