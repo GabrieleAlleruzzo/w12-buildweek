7534bd822c751b77216611640f6f2c45d8d93523
Vincenza <vinni2690@hotmail.com>
2025-04-22 14:55:18 +0200

    dropdown

--- src/components/Esperienze.jsx
+++ src/components/Esperienze.jsx
@@ -17,9 +17,8 @@ const Esperienze = function () {
                             </Dropdown.Toggle>
 
                             <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                <Dropdown.Item href="#/action-1">Aggiungi posizione lavorativa</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Aggiungi pausa lavorativa</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                         
