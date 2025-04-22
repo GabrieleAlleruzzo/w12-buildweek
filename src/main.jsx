a454482a55ef3b085927b4170fa79b65f6fff000
Vincenza <vinni2690@hotmail.com>
2025-04-22 12:28:03 +0200

    icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/index.js";
 import { BrowserRouter } from "react-router-dom";
